b0a5951f1b5c61143c41b36d427367f09c550855
AonanLi <ali@tetrad.com>
2019-08-08 21:33:43 -0700

    feat(preview): implement preview panel

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 
 import { pink3 } from '../styles/colors';
 
 const Button = ({ label, onPress }) => (
-    <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text style={styles.text}>{label}</Text>
-    </TouchableOpacity>
+    <View style={styles.view}>
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+            <Text style={styles.text}>{label}</Text>
+        </TouchableOpacity>
+    </View>
 );
 
 const styles = StyleSheet.create({
+    view: {
+        flexDirection: 'row'
+    },
     button: {
         borderBottomWidth: 1,
         borderColor: pink3,
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,15 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { View, Text, StyleSheet } from 'react-native';
+import _ from 'lodash';
 
 import Page from '../../components/Page';
+import Button from '../../components/Button';
 import FlavourPicker from './FlavourPicker';
 
 import { editOrder } from '../../actions/orders';
 
+import flavours from '../../util/flavours';
 import { padding } from '../../styles/styleguide';
 import { pink1, brown3, white, brown4 } from '../../styles/colors';
 
@@ -14,14 +17,39 @@ const height = 264;
 
 class Home extends PureComponent {
     render() {
+        const { ordered, editOrder } = this.props;
+        const sorted = _.sortBy(
+            _.map(ordered, (o, key) => ({
+                key,
+                amount: o
+            })),
+            'key'
+        );
+        const total = _.reduce(
+            ordered,
+            (r, v, k) => r + v * flavours.find(f => f.key === k).price,
+            0
+        ).toFixed(2);
+        const hasOrder = sorted.length > 0;
         return (
             <Page style={styles.background}>
                 <View style={styles.top}>
                     <Text style={styles.title}>Hey, Polar</Text>
                     <Text style={styles.subtitle}>We have smth yummy for you</Text>
                 </View>
-                <FlavourPicker {...this.props} />
-                <View style={styles.panel} />
+                <FlavourPicker ordered={ordered} editOrder={editOrder} />
+                <View style={styles.panel}>
+                    <Text style={styles.panelTitle}>Your Choice</Text>
+                    <View style={styles.list}>
+                        {sorted.map(o => (
+                            <View key={o.key}>
+                                <Text style={styles.order}>{`${o.amount} x ${o.key}`}</Text>
+                            </View>
+                        ))}
+                    </View>
+                    {hasOrder && <Text style={styles.total}>{`Total: $${total}`}</Text>}
+                    {hasOrder && <Button label="Order Now" />}
+                </View>
             </Page>
         );
     }
@@ -51,7 +79,24 @@ const styles = StyleSheet.create({
         backgroundColor: pink1,
         borderTopLeftRadius: padding,
         borderTopRightRadius: padding,
-        padding
+        paddingHorizontal: padding,
+        paddingVertical: 16,
+        justifyContent: 'space-between'
+    },
+    panelTitle: {
+        fontSize: 26,
+        color: brown4
+    },
+    list: {
+        height: 60
+    },
+    order: {
+        fontSize: 20,
+        color: brown3
+    },
+    total: {
+        fontSize: 20,
+        color: brown4
     }
 });
 
